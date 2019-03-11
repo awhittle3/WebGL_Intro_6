f81e94033ad75eb7fc722b46b6c1a4738edda894
Andrew Whittle <awhittle@ualberta.ca>
2019-03-11 09:08:56 -0600

    Initial commit

---
+++ examples/example02.js
@@ -0,0 +1,514 @@
+main();
+
+/************************************
+ * MAIN
+ ************************************/
+
+function main() {
+
+    console.log("Setting up the canvas");
+
+    // Find the canavas tag in the HTML document
+    const canvas = document.querySelector("#exampleCanvas");
+
+    // Initialize the WebGL2 context
+    var gl = canvas.getContext("webgl2");
+ 
+    // Only continue if WebGL2 is available and working
+    if (gl === null) {
+        printError('WebGL 2 not supported by your browser',
+            'Check to see you are using a <a href="https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API#WebGL_2_2" class="alert-link">modern browser</a>.');
+        return;
+    }
+
+    // Create a state for our scene
+    var state = {
+        camera: {
+            position: vec3.fromValues(0.0, 0.0, 5.0),
+            center: vec3.fromValues(0.0, 0.0, 0.0),
+            up: vec3.fromValues(0.0, 1.0, 0.0),
+        },
+        lights: [
+            {
+                position: vec3.fromValues(0.0, 5.0, 5.0),
+                colour: vec3.fromValues(1.0, 1.0, 1.0),
+                strength: 10.0,
+            }
+        ],
+        objects: [
+            {
+                model: {
+                    position: vec3.fromValues(0.0, 0.0, 0.0),
+                    rotation: mat4.create(), // Identity matrix
+                    scale: vec3.fromValues(1.5, 1.5, 1.5),
+                },
+                programInfo: simpleShader(gl),
+                buffers: null,
+                texture: null,
+                colour: vec4.fromValues(1.0, 0.2, 0.3, 0.8),
+            },
+            {
+                model: {
+                    position: vec3.fromValues(1.0, 1.0, 1.0),
+                    rotation: mat4.create(), // Identity matrix
+                    scale: vec3.fromValues(0.5, 0.5, 0.5),
+                },
+                programInfo: simpleShader(gl),
+                buffers: null,
+                texture: null,
+                colour: vec4.fromValues(0.1, 0.2, 0.3, 0.4),
+            },
+            {
+                model: {
+                    position: vec3.fromValues(-1.0, -1.0, -1.0),
+                    rotation: mat4.create(), // Identity matrix
+                    scale: vec3.fromValues(0.5, 0.5, 0.5),
+                },
+                programInfo: simpleShader(gl),
+                buffers: null,
+                texture: null,
+                colour: vec4.fromValues(0.1, 1.0, 0.3, 1.0),
+            },
+        ],
+        canvas: canvas,
+        selectedIndex: 0,
+    };
+
+    state.objects.forEach((object) => {
+        initQuadBuffers(gl, object);
+    });
+
+    console.log(state)
+    setupKeypresses(state);
+
+    console.log("Starting rendering loop");
+    startRendering(gl, state);
+}
+
+
+/************************************
+ * RENDERING CALLS
+ ************************************/
+
+function startRendering(gl, state) {
+    // A variable for keeping track of time between frames
+    var then = 0.0;
+
+    // This function is called when we want to render a frame to the canvas
+    function render(now) {
+        now *= 0.001; // convert to seconds
+        const deltaTime = now - then;
+        then = now;
+
+        updateState(deltaTime, state);
+
+        // Draw our scene
+        drawScene(gl, state);
+
+        // Request another frame when this one is done
+        requestAnimationFrame(render);
+    }
+
+    // Draw the scene
+    requestAnimationFrame(render);
+}
+
+function updateState(deltaTime, state) {
+    // Update state as you wish here.  Gets called every frame.
+    state.objects.forEach((object) => {
+
+    });
+}
+
+function drawScene(gl, state) {
+    // Set clear colour
+    // This is a Red-Green-Blue-Alpha colour
+    // See https://en.wikipedia.org/wiki/RGB_color_model
+    // Here we use floating point values. In other places you may see byte representation (0-255).
+    gl.clearColor(0.55686, 0.54902, 0.52157, 1.0);
+
+    gl.depthMask(false);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    // Clear the color and depth buffer with specified clear colour.
+    // This will replace everything that was in the previous frame with the clear colour.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var sortedObjects = state.objects.sort((a, b) => {
+        // TODO: Add an object comparison function
+        return -1;
+    });
+
+    sortedObjects.forEach((object) => {
+        // Choose to use our shader
+        gl.useProgram(object.programInfo.program);
+
+            // Update uniforms
+        {
+            var projectionMatrix = mat4.create();
+            var fovy = 60.0 * Math.PI / 180.0; // Vertical field of view in radians
+            var aspect = state.canvas.clientWidth / state.canvas.clientHeight; // Aspect ratio of the canvas
+            var near = 0.1; // Near clipping plane
+            var far = 100.0; // Far clipping plane
+            // Generate the projection matrix using perspective
+            mat4.perspective(projectionMatrix, fovy, aspect, near, far);
+
+            gl.uniformMatrix4fv(object.programInfo.uniformLocations.projection, false, projectionMatrix);
+        
+            var viewMatrix = mat4.create();
+            mat4.lookAt(
+                viewMatrix,
+                state.camera.position,
+                state.camera.center,
+                state.camera.up,
+            );
+            gl.uniformMatrix4fv(object.programInfo.uniformLocations.view, false, viewMatrix);
+
+
+            // Update model transform
+
+            var modelMatrix = mat4.create();
+            mat4.translate(modelMatrix, modelMatrix, object.model.position);
+            mat4.mul(modelMatrix, modelMatrix, object.model.rotation);
+            mat4.scale(modelMatrix, modelMatrix, object.model.scale);
+            gl.uniformMatrix4fv(object.programInfo.uniformLocations.model, false, modelMatrix);
+
+            gl.uniform4fv(object.programInfo.uniformLocations.colour, object.colour);
+        }
+
+        // Draw 
+        {
+            // Bind the buffer we want to draw
+            gl.bindVertexArray(object.buffers.vao);
+
+            // Draw the object
+            const offset = 0; // Number of elements to skip before starting
+            gl.drawElements(gl.TRIANGLES, object.buffers.numVertices, gl.UNSIGNED_SHORT, offset);
+        }
+
+    });
+}
+
+
+/************************************
+ * UI EVENTS
+ ************************************/
+
+function setupKeypresses(state){
+    document.addEventListener("keydown", (event) => {
+        console.log(event.code);
+
+        var object = state.objects[state.selectedIndex];
+
+        switch(event.code) {
+        case "KeyA":
+            state.usingGoodShader = !state.usingGoodShader;
+            state.objects.forEach((object) => {
+                object.programInfo = (state.usingGoodShader) ? state.goodShader : state.badShader;
+            });
+            break;
+        default:
+            break;
+        }
+    });
+}
+
+/************************************
+ * SHADER SETUP
+ ************************************/
+
+function simpleShader(gl){
+
+    // Vertex shader source code
+    const vsSource =
+    `#version 300 es
+    in vec3 aPosition;
+
+    uniform mat4 uProjectionMatrix;
+    uniform mat4 uViewMatrix;
+    uniform mat4 uModelMatrix;
+
+    uniform vec4 uColor;
+
+    out vec4 oColor;
+
+    void main() {
+        gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
+
+        oColor = uColor;
+    }
+    `;
+
+    // Fragment shader source code
+    const fsSource =
+    `#version 300 es
+    precision highp float;
+
+    out vec4 fragColor;
+
+    in vec4 oColor;
+
+    void main() {
+        fragColor = oColor;
+    }
+    `;
+
+
+    // Create our shader program with our custom function
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    // Collect all the info needed to use the shader program.
+    const programInfo = {
+        // The actual shader program
+        program: shaderProgram,
+        // The attribute locations. WebGL will use there to hook up the buffers to the shader program.
+        // NOTE: it may be wise to check if these calls fail by seeing that the returned location is not -1.
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, 'aPosition'),
+        },
+        uniformLocations: {
+            projection: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            view: gl.getUniformLocation(shaderProgram, 'uViewMatrix'),
+            model: gl.getUniformLocation(shaderProgram, 'uModelMatrix'),
+            colour: gl.getUniformLocation(shaderProgram, 'uColor'),
+        },
+    };
+
+       // Check to see if we found the locations of our uniforms and attributes
+    // Typos are a common source of failure
+    if (programInfo.attribLocations.vertexPosition === -1 ||
+        programInfo.uniformLocations.projection === -1 ||
+        programInfo.uniformLocations.view === -1 ||
+        programInfo.uniformLocations.model === -1 ||
+        programInfo.uniformLocations.color === -1 ) {
+        printError('Shader Location Error', 'One or more of the uniform and attribute variables in the shaders could not be located');
+    }
+
+    return programInfo;
+}
+
+
+/************************************
+ * BUFFER SETUP
+ ************************************/
+
+function initQuadBuffers(gl, object) {
+
+     // We have 3 vertices with x, y, and z values
+     const positionArray = new Float32Array([
+        -1.0, -1.0,  0.0,
+        1.0, -1.0,  0.0,
+        1.0,  1.0,  0.0,
+        -1.0,  1.0,  0.0,
+    ]);
+
+    const normalArray = new Float32Array([
+        // Front
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+    ]);
+
+    // We are using gl.UNSIGNED_SHORT to enumerate the indices
+    const indicesArray = new Uint16Array([
+        0,  1,  2,      0,  2,  3,
+    ]);
+
+    initBuffers(gl, object, positionArray, normalArray, null, null, indicesArray);
+}
+
+function initBuffers(gl, object, positionArray, normalArray, colourArray, textureCoordArray, indicesArray) {
+
+    // We have 3 vertices with x, y, and z values
+    const positions = new Float32Array(positionArray);
+
+    const normals = new Float32Array(normalArray);
+
+    const colours = new Float32Array(colourArray);
+
+    const textureCoords = new Float32Array(textureCoordArray);
+
+    // We are using gl.UNSIGNED_SHORT to enumerate the indices
+    const indices = new Uint16Array(indicesArray);
+
+    // Allocate and assign a Vertex Array Object to our handle
+    var vertexArrayObject = gl.createVertexArray();
+
+    // Bind our Vertex Array Object as the current used object
+    gl.bindVertexArray(vertexArrayObject);
+
+    object.buffers = {
+        vao: vertexArrayObject,
+        attributes: {
+            position: initPositionAttribute(gl, object.programInfo, positions),
+            normal: initNormalAttribute(gl, object.programInfo, normals),
+        },
+        indices: initIndexBuffer(gl, indices),
+        numVertices: indices.length,
+    };
+}
+
+function initPositionAttribute(gl, programInfo, positionArray) {
+    if(positionArray != null && positionArray.length > 0 && programInfo.attribLocations.vertexPosition != null){
+        // Create a buffer for the positions.
+        const positionBuffer = gl.createBuffer();
+
+        // Select the buffer as the one to apply buffer
+        // operations to from here out.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Now pass the list of positions into WebGL to build the
+        // shape. We do this by creating a Float32Array from the
+        // JavaScript array, then use it to fill the current buffer.
+        gl.bufferData(
+            gl.ARRAY_BUFFER, // The kind of buffer this is
+            positionArray, // The data in an Array object
+            gl.STATIC_DRAW // We are not going to change this data, so it is static
+        );
+
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute.
+        {
+            const numComponents = 3; // pull out 3 values per iteration, ie vec3
+            const type = gl.FLOAT; // the data in the buffer is 32bit floats
+            const normalize = false; // don't normalize between 0 and 1
+            const stride = 0; // how many bytes to get from one set of values to the next
+            // Set stride to 0 to use type and numComponents above
+            const offset = 0; // how many bytes inside the buffer to start from
+
+
+            // Set the information WebGL needs to read the buffer properly
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexPosition,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset
+            );
+            // Tell WebGL to use this attribute
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexPosition);
+        }
+
+        return positionBuffer;
+    }
+}
+
+
+function initColourAttribute(gl, programInfo, colourArray) {
+
+    if(colourArray != null && colourArray.length > 0 && programInfo.attribLocations.vertexColour != null) {
+        // Create a buffer for the positions.
+        const colourBuffer = gl.createBuffer();
+
+        // Select the buffer as the one to apply buffer
+        // operations to from here out.
+        gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
+
+        // Now pass the list of positions into WebGL to build the
+        // shape. We do this by creating a Float32Array from the
+        // JavaScript array, then use it to fill the current buffer.
+        gl.bufferData(
+            gl.ARRAY_BUFFER, // The kind of buffer this is
+            colourArray, // The data in an Array object
+            gl.STATIC_DRAW // We are not going to change this data, so it is static
+        );
+
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute.
+        {
+            const numComponents = 4; // pull out 4 values per iteration, ie vec4
+            const type = gl.FLOAT; // the data in the buffer is 32bit floats
+            const normalize = false; // don't normalize between 0 and 1
+            const stride = 0; // how many bytes to get from one set of values to the next
+            // Set stride to 0 to use type and numComponents above
+            const offset = 0; // how many bytes inside the buffer to start from
+
+            // Set the information WebGL needs to read the buffer properly
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexColour,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset
+            );
+            // Tell WebGL to use this attribute
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexColour);
+        }
+
+        return colourBuffer;
+    }
+}
+
+
+function initNormalAttribute(gl, programInfo, normalArray) {
+    if(normalArray != null && normalArray.length > 0 && programInfo.attribLocations.vertexNormal != null){
+        // Create a buffer for the positions.
+        const normalBuffer = gl.createBuffer();
+
+        // Select the buffer as the one to apply buffer
+        // operations to from here out.
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+
+        // Now pass the list of positions into WebGL to build the
+        // shape. We do this by creating a Float32Array from the
+        // JavaScript array, then use it to fill the current buffer.
+        gl.bufferData(
+            gl.ARRAY_BUFFER, // The kind of buffer this is
+            normalArray, // The data in an Array object
+            gl.STATIC_DRAW // We are not going to change this data, so it is static
+        );
+
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute.
+        {
+            const numComponents = 3; // pull out 4 values per iteration, ie vec3
+            const type = gl.FLOAT; // the data in the buffer is 32bit floats
+            const normalize = false; // don't normalize between 0 and 1
+            const stride = 0; // how many bytes to get from one set of values to the next
+            // Set stride to 0 to use type and numComponents above
+            const offset = 0; // how many bytes inside the buffer to start from
+
+            // Set the information WebGL needs to read the buffer properly
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexNormal,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset
+            );
+            // Tell WebGL to use this attribute
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexNormal);
+        }
+
+        return normalBuffer;
+    }
+}
+
+function initIndexBuffer(gl, elementArray) {
+
+    // Create a buffer for the positions.
+    const indexBuffer = gl.createBuffer();
+
+    // Select the buffer as the one to apply buffer
+    // operations to from here out.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    // Now pass the list of positions into WebGL to build the
+    // shape. We do this by creating a Float32Array from the
+    // JavaScript array, then use it to fill the current buffer.
+    gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER, // The kind of buffer this is
+        elementArray, // The data in an Array object
+        gl.STATIC_DRAW // We are not going to change this data, so it is static
+    );
+
+    return indexBuffer;
+}
---
+++ materials/README.md
@@ -0,0 +1,3 @@
+# Textures
+
+These textures were obtained from [CC0Textures](https://cc0textures.com) under the [CC0 License](https://creativecommons.org/publicdomain/zero/1.0/).
